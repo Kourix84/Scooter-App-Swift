7869002549cb1739578df7544e3b9eab7c53a471
Kourosh Esmaeil Tajer <166245580+Kourix84@users.noreply.github.com>
2025-10-30 11:39:37 +0100

    First Push for Swift App

---
+++ Scooter App/Scooter AppTests/Scooter_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Scooter_AppTests.swift
+//  Scooter AppTests
+//
+//  Created by Kourosh Esmaeil Tajer on 30/10/2025.
+//
+
+import Testing
+@testable import Scooter_App
+
+struct Scooter_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
